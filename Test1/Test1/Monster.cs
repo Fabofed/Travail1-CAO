cdaa193fbb86cebb16787ca4561e21ba0fbeea6e
Nemesis01 <stephane.nton@gmail.com>
2016-10-24 13:06:32 -0400

    Création classe MONSTER Stéphane Nton

---
+++ Test1/Test1/Monster.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Monster
+    {
+
+        public Monster()
+        {
+
+        }
+
+    }
+}
