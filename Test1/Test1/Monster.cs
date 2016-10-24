d588f76cd996a678a8aac6fe966660d84a638a7b
Nemesis01 <stephane.nton@gmail.com>
2016-10-24 17:21:35 -0400

    Modif classe monstre Stéphane

--- Test1/Test1/Monster.cs
+++ Test1/Test1/Monster.cs
@@ -6,13 +6,56 @@ using System.Threading.Tasks;
 
 namespace Test1
 {
-    class Monster
+
+    enum TypeMonstre { Feu, Magma, Terre, Vegetation, Eau, Glace, Air, Electricite }
+
+    class Monstre
     {
 
-        public Monster()
+        private int id;
+        private byte rarete;
+        private byte experience;
+        private int pointsExperience;
+        private Caracteristiques caracteristiques;
+
+        #region Propriétés (Acesseurs et Mutateurs)
+
+        public int Id { get; set; }
+        public string Nom { get; }
+        public string Surnom { get; set; }
+        public TypeMonstre Type { get; protected set; }
+        public byte Rarete { get; set; }
+        public byte Experience { get; protected set; }
+        public int PointsExperience
+        {
+            get
+            {
+                return pointsExperience;
+            }
+
+            set
+            {
+                pointsExperience = value;
+            }
+        }
+
+
+
+
+        #endregion
+
+        public Monstre()
         {
 
         }
 
     }
+
+    struct Caracteristiques
+    {
+
+        private int pointsDeVieDeBase;
+        private int pointsDEnergie;
+    }
+
 }
