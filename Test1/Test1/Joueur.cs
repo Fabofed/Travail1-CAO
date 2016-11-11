7490e38f4e31f5d879822b67061c7f0c55779fe1
Dave <fabofedd@gmail.com>
2016-11-10 21:35:13 -0500

    Rajout de certaines classes

---
+++ Test1/Test1/Joueur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravailSession
+{
+    class Joueur
+    {
+        public String nom;
+        public int argent;
+        Inventaire inventaire = new Inventaire();
+        Monstre monstre = new Monstre();
+        static Monstre[] Equipe = new Monstre[5];
+        static Monstre[] monstreCaptures = new Monstre[10];
+
+    }
+}
